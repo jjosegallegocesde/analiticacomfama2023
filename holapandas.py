dc0a3057344fa43b7e65e6a7d5c942419f2f633c
BerMira <bamg359@gmail.com>
2023-01-25 11:14:43 -0500

    se crea ejemplo de pandas

---
+++ holapandas.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+#cargar datos para poderlos analizar
+
+#1. es declarar una variable donde almaceno la fuente de datos
+fuenteDatos=pd.read_csv("./empleados.csv")
+
+#2. obtener N cantidad de datos (FILTRO)
+filtrohead=fuenteDatos.head()
+print(filtrohead)
+
+filtrohead2=fuenteDatos.head(15)
+print("\n")
+print(filtrohead2)
+
+filtrotail=fuenteDatos.tail(30)
+print("\n")
+print(filtrotail)
+
+filtroNombre=fuenteDatos["nombres"].head(10)
+print("\n")
+print(filtroNombre)
+
+filtroIloc=fuenteDatos.iloc[[5,15,30]]
+print("\n")
+print(filtroIloc)
