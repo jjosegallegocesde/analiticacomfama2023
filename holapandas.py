07f70572b9826dc88b13d7d1520f5b67afe7bb84
Daniela Oquendo Tabares <DanielaOquendo@comfama.com.co>
2023-01-25 11:45:45 -0500

    se guarda reto

--- holapandas.py
+++ holapandas.py
@@ -24,3 +24,25 @@ print(filtroNombre)
 filtroIloc=fuenteDatos.iloc[[5,15,30]]
 print("\n")
 print(filtroIloc)
+
+
+#1. priemros 50 empleados
+filtrohead3=fuenteDatos.head(50)
+print("\n")
+print(filtrohead3)
+
+filtroNomEmpleados=fuenteDatos.head(50)["salario"]
+print("\n")
+print(filtroNomEmpleados)
+
+filtroMedia=fuenteDatos.describe()
+print("\n")
+print(filtroMedia)
+
+filtroIloc=fuenteDatos.iloc[[100,200,300,400]]
+print("\n")
+print(filtroIloc)
+
+filtrotail=fuenteDatos.tail(3)[["salario","nombres"]]
+print("\n")
+print(filtrotail)
