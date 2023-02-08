0306f205b69ff7c0dec894d0b2779561c6f04168
SebastianEspitiaS <espitiasanchezsebastian64@gmail.com>
2023-02-08 01:03:24 -0500

    practica pandas

---
+++ practicas pandas2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+extranjeros = pd.read_csv("./exterior.csv")
+
+print(extranjeros["Área Conocimiento"])
+print("")
+print(extranjeros.head(20))
+print("")
+print(extranjeros.tail(35))
+print("")
+print(extranjeros.head(1000).mean())
+print("")
+print(extranjeros.columns)
